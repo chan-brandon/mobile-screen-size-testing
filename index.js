71922785cc9750059104c1eeff18b02e5567609a
Brandon Chan <brandonhfchan@gmail.com>
2019-02-04 20:40:15 -0500

    First commit for mobile screen tests

---
+++ index.js
@@ -0,0 +1,22 @@
+// jshint esversion:6
+
+const express = require("express");
+const app = express();
+const request = require('request');
+const bodyParser = require('body-parser');
+
+app.get("/", function(req, response){
+  response.send("Hello, world!");
+});
+
+app.get("/contact", function(req, response){
+  response.send("This is a contact page!");
+});
+
+app.get("/about", function(req,response){
+  response.send("Brandon owns this website!");
+});
+
+app.get("/movie", function(req, response){
+  response.send("This is a testing page.");
+});
